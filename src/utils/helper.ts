655000b7f69a6d5dd50fd6fc20cbb95ce6fd7722
akhil-neoito <akhil.chandran@neoito.com>
2023-02-21 19:12:05 +0530

    refactor: readability

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -11,15 +11,24 @@ export const HR = (
   return chalk[color](char.repeat(length));
 };
 
-export const envFileNotFoundError = (key: CommonEnvKeys) => {
+const createEnvSnippet = (fileName: string) => {
+  const scriptChalk = chalk.bgBlueBright.bold;
+  return `${scriptChalk(` cp .env.example ${fileName} `)}`;
+};
+
+export const envFileNotFoundError = (key: CommonEnvKeys): string => {
   const fileNameMessage = `${chalk.yellowBright(
     EnvironmentFile.DEFAULT
-  )} and ${chalk.yellowBright(EnvironmentFile[key])} files not found`;
+  )} or ${chalk.yellowBright(EnvironmentFile[key])} is required`;
+  const envNotFoundMessage = chalk.red.bold('Environment file not found!!');
+  const divider = HR('red', '-', 40);
   return `
-    \r${HR('red', '-', 50)}\n
+    \r${divider}\n
+    \r${envNotFoundMessage}\n
     \r${fileNameMessage}\n
-    \rOne of them is required\n
-    \rTry ${chalk.bgCyan.bold('cp .env.example .env')}\n
-    \r${HR('red', '-', 50)}
+    \rTry the following:\n
+    \r${createEnvSnippet(EnvironmentFile[key])}\n
+    \r${createEnvSnippet(EnvironmentFile.DEFAULT)}\n
+    \r${divider}
   `;
 };
