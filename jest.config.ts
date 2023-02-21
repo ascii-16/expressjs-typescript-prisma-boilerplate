5ec5f416a4ed6c02102ce9c9e36d9b62177afced
akhil-neoito <akhil.chandran@neoito.com>
2023-02-21 15:44:41 +0530

    chore(test): add testRegex for jest

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,10 @@
 import { type Config } from 'jest';
 
+const testRegex = '(/__tests__/.*|(\\.|/)(test|spec))\\.(js?|ts?)?$';
 const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testRegex,
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
