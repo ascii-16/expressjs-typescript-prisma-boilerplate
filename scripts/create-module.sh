bd4164e9a2b9c79d9b6807896730842965d4d2e5
akhil-neoito <akhil.chandran@neoito.com>
2023-03-02 01:02:05 +0530

    refactor: remove unwanted semicolon

--- scripts/create-module.sh
+++ scripts/create-module.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-echo "Module name? ";
-read -r module_name;
+echo "Module name? "
+read -r module_name
 
 # Creating folder
-mkdir -p src/modules/"$module_name";
-echo "✅ Created module folder";
+mkdir -p src/modules/"$module_name"
+echo "✅ Created module folder"
 
 # Creating files
-cd src/modules/"$module_name" || exit;
-touch "$module_name".controller.ts "$module_name".service.ts "$module_name".route.ts;
-echo "✅ Created controller, service, and routes";
+cd src/modules/"$module_name" || exit
+touch "$module_name".controller.ts "$module_name".service.ts "$module_name".route.ts
+echo "✅ Created controller, service, and routes"
