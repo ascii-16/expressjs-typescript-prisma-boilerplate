fcd82934cc4d5d0b337643b7da69bda87b84d1ea
akhil-neoito <akhil.chandran@neoito.com>
2023-08-08 03:10:32 +0530

    feat(ts-5): add example decorator

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,7 +1,9 @@
 import { type users } from '@prisma/client';
 import prisma from '@/lib/prisma';
+import LogMessage from '@/decorators/log-message.decorator';
 
 export default class UserService {
+  @LogMessage({ message: 'test-decorator' })
   public async createUser(data: users) {
     const user = await prisma.users.create({ data });
     return user;
