6d52b505028c5293bd585ab037ab5115b8a1168d
vintagegnome <akhil.chandran@neoito.com>
2023-08-08 07:03:00 +0530

    refactor(homepage): update styles

--- public/js/home.js
+++ public/js/home.js
@@ -2,6 +2,6 @@ document.getElementById('go-back').addEventListener('click', () => {
   history.back();
 });
 
-// document.getElementById('leave').addEventListener('click', () => {
-//   window.location.href = 'https://youtu.be/dQw4w9WgXcQ';
-// });
+document.getElementById('leave').addEventListener('click', () => {
+  window.location.href = 'https://youtu.be/dQw4w9WgXcQ';
+});
